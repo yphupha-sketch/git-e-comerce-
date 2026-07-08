dec96cc9e1e60b51a22f5a29f1ac6d8e7334a27f
phupha <y.phupha@gmail.com>
2026-07-08 22:32:44 +0700

    add user database

---
+++ 05_mongodb-schema_users.jason.js
@@ -0,0 +1,37 @@
+// MongoDB Playground
+// Use Ctrl+Space inside a snippet or a string literal to trigger completions.
+
+// The current database to use.
+use('my_ecommerce_db');
+
+// Create a new document in the collection.
+db.getCollection('customers ').insertOne({
+
+});
+
+[
+  {
+    "id": 1,
+    "name": "สมชาย ใจดี",
+    "address": "123 ถ.สุขุมวิท แขวงคลองเตย เขตคลองเตย กรุงเทพฯ 10110",
+    "phone": "081-234-5678",
+    "username": "somchai",
+    "password": "pass1234"
+  },
+  {
+    "id": 2,
+    "name": "วิไล สมานมิตร",
+    "address": "45/6 หมู่ 3 ต.หนองหอย อ.เมือง จ.เชียงใหม่ 50000",
+    "phone": "082-345-6789",
+    "username": "wilai",
+    "password": "pass5678"
+  },
+  {
+    "id": 3,
+    "name": "ปรีชา รักดี",
+    "address": "88 ถ.ศรีภูวนารถ ต.หาดใหญ่ อ.หาดใหญ่ จ.สงขลา 90110",
+    "phone": "083-456-7890",
+    "username": "preecha",
+    "password": "pass9012"
+  }
+]
